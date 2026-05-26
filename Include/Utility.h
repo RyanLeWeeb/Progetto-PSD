6cba9d6e10143627a9ce6a72a33c8b0c43ded7d3
RyanKunn <alessandrorusso2006@gmail.com>
2026-05-26 14:21:49 +0200

    Utility update

--- Include/Utility.h
+++ Include/Utility.h
@@ -9,8 +9,14 @@ typedef struct {
 } Data;
 
 short confrontaDate(Data d1, Data d2);
+//    - Pre: nessuna
+//
+//    - Post: la funzione restituisce 0 se le date sono uguali, 1 altrimenti
 
 short sonoOrariSovrapposti(int inizio1, int fine1, int inizio2, int fine2);
+//    - Pre: nessuna
+//
+//    - Post: la funzione restituisce 1 se gli orari si sovrappongono, 0 altrimenti
 
 
 #endif
